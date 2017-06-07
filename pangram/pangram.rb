486ac09a2ba4ad3388b3e34a9622df2a1f42c331
Marcin Szulc <maszudev@gmail.com>
2017-06-07 08:39:45 +0000

    Add pangram exercise

--- pangram/pangram.rb
+++ pangram/pangram.rb
@@ -5,7 +5,12 @@ end
 class Pangram
   
   def self.pangram?(phrase)
-    
+    if phrase.empty?
+      false
+    else
+    remaining_letters = ("a".."z").to_a - phrase.downcase.scan(/[a-zA-Z\d*]/)
+    true if remaining_letters.empty?
+    end
   end
   
 end
