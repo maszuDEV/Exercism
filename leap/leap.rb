290e8c9cfae1c24ca74db351074089f14ab556dc
Marcin Szulc <maszudev@gmail.com>
2017-06-25 12:30:09 +0000

    Refactored leab solution

--- leap/leap.rb
+++ leap/leap.rb
@@ -4,6 +4,6 @@ end
 
 class Year
   def self.leap?(year)
-    ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) ? true : false
+    year % 4 == 0 && year % 100 != 0 || year % 400 == 0 
   end
 end
